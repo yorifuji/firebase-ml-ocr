1c1ff902252c55fe9baf184805e785dca3458c28
yorifuji <yorifuji@gmail.com>
2020-12-19 22:48:43 +0900

    Initial Commit

---
+++ firebase-ml-ocr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  firebase-ml-ocr
+//
+//  Created by yorifuji on 2020/12/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ firebase-ml-ocr/firebase_ml_ocrApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebase_ml_ocrApp.swift
+//  firebase-ml-ocr
+//
+//  Created by yorifuji on 2020/12/19.
+//
+
+import SwiftUI
+
+@main
+struct firebase_ml_ocrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
